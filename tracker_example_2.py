0523e1d8bc8dd4aa1c70335dba04f0b4ab4f16e5
Aman <30496447+aman2575@users.noreply.github.com>
2021-07-29 16:19:00 +0530

    Add files via upload

---
+++ tracker_example_2.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2
+
+
+def nothing(x):
+    print(x)
+
+cv2.namedWindow('image')
+
+cv2.createTrackbar('CP', 'image', 10, 400, nothing)
+
+switch = 'color/gray'
+cv2.createTrackbar(switch, 'image', 0, 1, nothing)
+
+while 1:
+    img = cv2.imread('image3.jpg')
+    #img = cv2.imshow('image', img)
+    pos = cv2.getTrackbarPos('CP', 'image')
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img, str(pos), (50, 200), font, 6, (0, 255, 255), 10)
+
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+    s = cv2.getTrackbarPos(switch, 'image')
+    if s == 0:
+        pass
+    else:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.imshow('image', img)
+cv2.destroyAllW1indows()
